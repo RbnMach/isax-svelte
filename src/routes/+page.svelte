2eff5cad70afe9a15cbc931854eb1dfbaf19d090
Ruben <ruben.machaca.q@gmail.com>
2024-06-06 02:43:13 -0400

    Readme init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-	import { Isax, Bulk } from "$lib/index.js";
+	import { Isax, Bold, Bulk, Linear, Outline, Twotone } from "$lib/index.js";
 	
 	let name = 'user';
 </script>
 
-<p><Isax {name} type="twotone" /> Isax</p>
+<p><Isax {name} type="twotone" size="2em" axis_y="0.5em" axis_x="0.5em" /> Isax</p>
 
-<p><Bulk name="home" /> Home</p>
+<p><Bold name="home" size="2em" axis_y="0.5em" axis_x="0.5em" /> Home</p>
+<p><Bulk name="home" size="2em" axis_y="0.5em" axis_x="0.5em" /> Home</p>
+<p><Linear name="home" size="2em" axis_y="0.5em" axis_x="0.5em" /> Home</p>
+<p><Outline name="home" size="2em" axis_y="0.5em" axis_x="0.5em" /> Home</p>
+<p><Twotone name="home" size="2em" axis_y="0.5em" axis_x="0.5em" /> Home</p>
 
 <button on:click={() => (name == 'user' ? (name = 'home') : (name = 'user'))}>Click</button>
 
